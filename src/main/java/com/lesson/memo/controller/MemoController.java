8c53798f5c5ae7bfd7859ac0b1f42d45694fd21c
hachi0109 <honeywisteria0109@gmail.com>
2026-02-06 14:38:06 +0900

    全文検索機能の実装

--- src/main/java/com/lesson/memo/controller/MemoController.java
+++ src/main/java/com/lesson/memo/controller/MemoController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lesson.memo.model.Memo;
@@ -30,9 +31,26 @@ public class MemoController {
     private MemoRepository memoRepository;
 
     @GetMapping
-    public String list(Model model) {
-    	List<Memo> memos = memoRepository.findAllByOrderByPriorityAsc();
+    public String list(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
+        if (keyword != null && !keyword.isBlank()) {
+            return "redirect:/memo/search?keyword=" + keyword;
+        }
+        List<Memo> memos = memoRepository.findAllByOrderByPriorityAsc();
         model.addAttribute("memos", memos);
+        return "memo-list";
+    }
+    
+    @GetMapping("/search")
+    public String search(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
+        List<Memo> memos;
+        if (keyword != null && !keyword.isBlank()) {
+            memos = memoRepository.findByTitleContainingOrContentContainingOrderByPriorityAsc(keyword, keyword);
+        } else {
+            return "redirect:/memo";
+        }
+        
+        model.addAttribute("memos", memos);
+        model.addAttribute("keyword", keyword);
         return "memo-list";
     }
 
--- src/main/java/com/lesson/memo/repository/MemoRepository.java
+++ src/main/java/com/lesson/memo/repository/MemoRepository.java
@@ -7,5 +7,6 @@ import com.lesson.memo.model.Memo;
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 	List<Memo> findAllByOrderByPriorityAsc();
+	List<Memo> findByTitleContainingOrContentContainingOrderByPriorityAsc(String title, String content);
     
 }
