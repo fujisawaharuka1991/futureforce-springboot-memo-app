e80b6a32fb638cb79a11050f591887329766e163
hachi0109 <honeywisteria0109@gmail.com>
2026-02-06 10:39:46 +0900

    priorityにNotnull制約追加、エラーメッセージ表示追加

--- src/main/java/com/lesson/memo/model/Memo.java
+++ src/main/java/com/lesson/memo/model/Memo.java
@@ -1,5 +1,4 @@
 package com.lesson.memo.model;
-
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
@@ -8,6 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -35,5 +35,7 @@ public class Memo {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updatedAt;
     
+    @NotNull(message = "優先度を選択してください")
+    @Column(nullable = false)
     private Priority priority;
 }
